4ae149a24263216e4ea7fcd3e249d768f990f136
mathieu <mathieu.kohl@students.hevs.ch>
2021-11-11 20:13:41 +0100

    adding the service firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth , createUserWithEmailAndPassword} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBKKBamDhPsS4DBxZ_Na1aCWybXwLPthPU",
+  authDomain: "qrfinges.firebaseapp.com",
+  projectId: "qrfinges",
+  storageBucket: "qrfinges.appspot.com",
+  messagingSenderId: "492229251310",
+  appId: "1:492229251310:web:3e07d76e9814bf090123ff",
+  measurementId: "G-MYBSS7VS0G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export function signup(email, password){
+    return createUserWithEmailAndPassword(auth, email, password);
+}
